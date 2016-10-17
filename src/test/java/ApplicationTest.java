800163f8dc13502f3ece953bfcb229e5822c0f41
Z2XifVoTXyezWnF <Twx-devops2@ptc.com>
2016-10-17 14:35:10 -0400

    st

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 @SpringApplicationConfiguration(Application.class)
 @WebIntegrationTest
 public class ApplicationTest {
-    private static FirefoxDriver driver;
+    private FirefoxDriver driver;
 
     @BeforeClass
     public static void setUp() throws IOException {
